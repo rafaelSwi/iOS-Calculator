a2fd76babf29f29cfc52cc96f33004953559a38a
Rafael Neuwirth Swierczynski <channelrns@gmail.com>
2022-08-08 20:13:25 -0300

    Initial Commit

---
+++ iOS Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iOS Calculator
+//
+//  Created by Rafael Neuwirth Swierczynski on 08/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS Calculator/iOS_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_CalculatorApp.swift
+//  iOS Calculator
+//
+//  Created by Rafael Neuwirth Swierczynski on 08/08/22.
+//
+
+import SwiftUI
+
+@main
+struct iOS_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
